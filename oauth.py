f1e361d5a6aa3452af9d075ea738898c427ee96e
Stephen Haywood <stephen@averagesecurityguy.info>
2013-03-15 09:33:12 -0400

    Added license.

--- oauth.py
+++ oauth.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012, AverageSecurityGuy
+# Copyright (c) 2013, ASG Consulting
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
@@ -11,7 +11,7 @@
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
 #
-#   Neither the name of AverageSecurityGuy nor the names of its contributors may
+#   Neither the name of ASG Consulting nor the names of its contributors may
 #   be used to endorse or promote products derived from this software without
 #   specific prior written permission.
 #
