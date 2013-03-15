35a51aafd10452b2f9e15eeb978513c152e78ad9
Stephen Haywood <stephen@averagesecurityguy.info>
2013-03-15 09:32:43 -0400

    Added license and usage.

--- kpt.rb
+++ kpt.rb
@@ -1,11 +1,45 @@
-##
-# This file is part of the Metasploit Framework and may be subject to
-# redistribution and commercial restrictions. Please see the Metasploit
-# web site for more information on licensing and terms of use.
-#   http://metasploit.com/
+# Copyright (c) 2013 ASG Consulting
+# All rights reserved.
 #
-##
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of ASG Consulting nor the names of its contributors may
+#  be used to endorse or promote products derived from this software without
+#  specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+# OF SUCH DAMAGE.
 
+# To use this module create a modules/auxiliary/analyze folder in your ~/.msf4 folder.
+# Copy the module into the folder and start msfconsole. Finally, load the module with
+# the use command:
+#
+# use auxiliary/analyze/kpt
+#
+# You will need to set the KPT_PATH option to the directory that holds the KnownPlainText
+# client files.
+#
+# set KPT_PATH /root/KnownPlainText
+#
+# Finally, run the module and it will grab all the smb_hashes from the metasploit database,
+# write them to a file, and call the KnownPlainText client with the file. Finally, it will
+# load back into the database any passwords cracked by KnownPlainText.
 
 require 'msf/core'
 
@@ -22,6 +56,7 @@ class Metasploit3 < Msf::Auxiliary
 				(hashdump). The goal of this module is to find trivial passwords in a short 
 				amount of time. To crack complex passwords another password cracking tool
 				should be used outside of Metasploit.
+
 				You must have a working copy of the KnownPlainText client including a 
 				valid license key for this module to work properly.
 			},
